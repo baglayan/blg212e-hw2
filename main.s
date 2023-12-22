c80323862886e8fc7513fb848eb16d8281cac557
baglayan <baglayan19@itu.edu.tr>
2023-12-22 15:43:21 +0300

    Add some comments

--- main.s
+++ main.s
@@ -16,7 +16,7 @@
                 
                 ALIGN                           ;align code within appropriate boundaries        
                 ENTRY                           ;declare as entry point
-                EXPORT __main                   ;declare __main symbol to be used by linker,
+                EXPORT __main                   ;declare __main symbol to be used by linker
         
 SYST_CSR        EQU 0xE000E010                  ;give symbolic name SYST_CSR to address of SysTick Control and Status Register
 SYST_RVR        EQU 0xE000E014                  ;give symbolic name SYST_RVR to address of SysTick Reload Value Register
@@ -25,9 +25,9 @@ SYST_CVR        EQU 0xE000E018                  ;give symbolic name SYST_CVR to 
 RELOAD          EQU 0xF9FFF                     ;give symbolic name RELOAD to reload value for 64 MHz and period of 16 ms
 START           EQU 2_111                       ;give symbolic name START to will set CLKSOURCE, TICKINT and ENABLE values of Control and Status Register to 1
 
-__main          FUNCTION                        ;declare function start
+__main          PROC                            ;declare start of procedure 'main'
 
-                BL timer_init
+                BL timer_init                   ;branch with link to timer_init procedure to initialize systick timer
                 
                 MOVS R0, #2                     ;set main loop iterator to 2
                 B m_cmp                         ;branch to loop comparison
@@ -36,15 +36,15 @@ m_body          BL save_start                   ;branch with link to save_start 
                 BL bubblesort                   ;branch with link to bubblesort procedure
                 BL save_exec                    ;branch with link to save_exec procedure to save execution time to memory
 
-m_cmp           CMP R0, #size
-                BLE m_body
+m_cmp           CMP R0, #size                   ;compare iterator with size
+                BLS m_body                      ;branch to m_body if R0 is lower than or same as size
         
         
-                BL timer_stop
+                BL timer_stop                   ;branch to timer_stop to stop systick timer
         
 stop            B    stop                       ;infinite loop to signify end of program
         
-                ENDFUNC
+                ENDP                            ;declare end of procedure
         
 timer_init      PROC                            ;declare start of procedure 'timer_init'
 
@@ -53,26 +53,26 @@ timer_init      PROC                            ;declare start of procedure 'tim
                 LDR R2, =RELOAD                 ;load RELOAD value into R2
                 LDR R3, =START                  ;load START value into R3
                 
-                STR    R2, [R1]                 ;store RELOAD value in Reload Value Register
+                STR R2, [R1]                    ;store RELOAD value in Reload Value Register
                 STR R3, [R0]                    ;store START value in Control and Status Register
                 
-				BX LR
+                BX LR                           ;branch back to caller
                 ENDP                            ;declare end of procedure
         
 timer_stop      PROC                            ;declare start of procedure 'timer_stop'
 
-                BX LR
+                BX LR                           ;branch back to caller
                 ENDP                            ;declare end of procedure
         
-save_start      PROC                        
+save_start      PROC                            ;declare start of procedure 'save_start'               
 
-                BX LR
-                ENDP
+                BX LR                           ;branch back to caller
+                ENDP                            ;declare end of procedure
         
-save_exec       PROC
+save_exec       PROC                            ;declare start of procedure 'save_exec'
 
-                BX LR
-                ENDP
+                BX LR                           ;branch back to caller
+                ENDP                            ;declare end of procedure
         
 bubblesort      PROC                            ;declare start of procedure 'bubblesort'. R0 is the iterator
 
@@ -80,46 +80,46 @@ bubblesort      PROC                            ;declare start of procedure 'bub
                 B    outer_loop_cmp             ;branch to the comparison for outer loop
         
 outer_loop_body SUBS R2, R0, #1                 ;R2 = j (iterator for the inner loop)
-                B    inner_loop_cmp
-                ADDS R1, R1, #1
+                B    inner_loop_cmp             ;branch to the comparison for inner loop
+                ADDS R1, R1, #1                 ;increment outer loop iterator
 
-inner_loop_body LDR R4, =array
-                LDR R4, [R4, #0]
+inner_loop_body LDR R4, =array                  ;load the address of 'array' into R4
+                LDR R4, [R4, R2]                ;load the jth element into R4
                 
-                LDR R5, =array
-                SUBS R7, R5, #1
-                LDR R5, [R6, #0]
+                LDR R5, =array                  ;load the address of 'array' into R5
+                SUBS R7, R2, #1                 ;load the value of j-1 into R7
+                LDR R5, [R5, R7]                ;load the j-1th element into R5
                 
-                CMP R4, R5
-                BCS inner_loop_cmp
+                CMP R4, R5                      ;compare R4 and R5
+                BCS inner_loop_cmp              ;branch to inner_loop_cmp if R4 is higher than or equal to R5
                 
-                BL swap_elements
+                BL swap_elements                ;branch with link to 'swap_elements' procedure to swap elements
                 
-                SUBS R2, R2, #1
+                SUBS R2, R2, #1                 ;decrement inner loop iterator
 
-inner_loop_cmp  CMP R2, R1
-                BCS    inner_loop_body
+inner_loop_cmp  CMP R2, R1                      ;compare inner loop iterator with outer loop iterator
+                BCS inner_loop_body             ;branch to inner_loop_body if 
 
-outer_loop_cmp  SUBS R3, R0, #1
-                CMP R1, R3
-                BCC outer_loop_body
+outer_loop_cmp  SUBS R3, R0, #1                 ;decrement main iterator R0 and put the value into R3
+                CMP R1, R3                      ;compare the outer loop iterator with main iterator - 1
+                BCC outer_loop_body             ;branch to outer_loop_body if R1 is lower than R3
 
-                BX LR
+                BX LR                           ;branch back to caller
                 ENDP                            ;declare end of procedure
         
-swap_elements   PROC
+swap_elements   PROC                            ;declare start of procedure 'swap_elements'
 
                 MOVS R6, R5
                 MOVS R5, R4
                 MOVS R4, R6
-				
+                
                 BX LR
-				ENDP
+                ENDP                            ;declare end of procedure
                 
         
-                AREA arraydata, DATA, READWRITE  ;declare data area
+                AREA arraydata, DATA, READWRITE ;declare data area
         
 array           DCD 0xa603e9e1, 0xb38cf45a, 0xf5010841, 0x32477961, 0x10bc09c5, 0x5543db2b, 0xd09b0bf1, 0x2eef070e, 0xe8e0e237, 0xd6ad2467, 0xc65a478b, 0xbd7bbc07, 0xa853c4bb, 0xfe21ee08, 0xa48b2364, 0x40c09b9f, 0xa67aff4e, 0x86342d4a, 0xee64e1dc, 0x87cdcdcc, 0x2b911058, 0xb5214bbc, 0xff4ecdd7, 0x3da3f26, 0xc79b2267, 0x6a72a73a, 0xd0d8533d, 0x5a4af4a6, 0x5c661e05, 0xc80c1ae8, 0x2d7e4d5a, 0x84367925, 0x84712f8b, 0x2b823605, 0x17691e64, 0xea49cba, 0x1d4386fb, 0xb085bec8, 0x4cc0f704, 0x76a4eca9, 0x83625326, 0x95fa4598, 0xe82d995e, 0xc5fb78cb, 0xaf63720d, 0xeb827b5, 0xcc11686d, 0x18db54ac, 0x8fe9488c, 0xe35cf1, 0xd80ec07d, 0xbdfcce51, 0x9ef8ef5c, 0x3a1382b2, 0xe1480a2a, 0xfe3aae2b, 0x2ef7727c, 0xda0121e1, 0x4b610a78, 0xd30f49c5, 0x1a3c2c63, 0x984990bc, 0xdb17118a, 0x7dae238f, 0x77aa1c96, 0xb7247800, 0xb117475f, 0xe6b2e711, 0x1fffc297, 0x144b449f, 0x6f08b591, 0x4e614a80, 0x204dd082, 0x163a93e0, 0xeb8b565a, 0x5326831, 0xf0f94119, 0xeb6e5842, 0xd9c3b040, 0x9a14c068, 0x38ccce54, 0x33e24bae, 0xc424c12b, 0x5d9b21ad, 0x355fb674, 0xb224f668, 0x296b3f6b, 0x59805a5f, 0x8568723b, 0xb9f49f9d, 0xf6831262, 0x78728bab, 0x10f12673, 0x984e7bee, 0x214f59a2, 0xfb088de7, 0x8b641c20, 0x72a0a379, 0x225fe86a, 0xd98a49f3
-size            EQU 0x64                           ;array size
+size            EQU 0x64                        ;array size
         
-                END                                ;declare the end of code
+                END                             ;declare the end of code
